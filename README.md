113f89090d3ec1bce3b3ef7392db0ffa53e1b359
Diogo Cruz <diogo.e.cruz@hotmail.com>
2022-05-04 00:34:29 +0100

    react integration

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ES
 This is the ES project
+
+### Run Server:
+
+1. First about react:
+ $ `cd frontend && npm run build`
+2. Second run server django:
+ $ `cd .. && python3 manage.py runserver`
+
+- Then access localhost:8000 for react app
+- Access localhost:8000/api for django app
+
+### For frontend testing:
+
+$ `cd frontend && npm run start`
+
+- Access localhost:3000 for react app live reload
--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'ebdjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "frontend/build")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,6 +69,10 @@ TEMPLATES = [
     },
 ]
 
+STATICFILES_DIRS = [
+  # Tell Django where to look for React's static files (css, js)
+  os.path.join(BASE_DIR, "frontend/build/static"),
+]
 WSGI_APPLICATION = 'ebdjango.wsgi.application'
 
 
@@ -115,9 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
 STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
