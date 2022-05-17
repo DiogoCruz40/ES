13614e0fe18d3bd92aaaee7968ecbe12db398316
Diogo Cruz <diogo.e.cruz@hotmail.com>
2022-05-17 17:56:26 +0100

    test implementation and fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
 
 function App() {
+
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:8000/items')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setItems(data);
+      })
+  }, [])
+
   return (
     <div className="App">
-     
+      {items && <BlogList blogs={items} />}
     </div>
   );
 }
