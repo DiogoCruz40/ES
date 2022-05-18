d56223438ce721b37dc1bbec08b37f538e6d2dad
Diogo Cruz <diogo.e.cruz@hotmail.com>
2022-05-18 02:40:03 +0100

    abort controll v2

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -22,7 +22,11 @@ const useFetch = (url) => {
         setError(null);
       })
       .catch((err) => {
+        if (err.name === 'AbortError') {
+          console.log('fetch aborted')
+        } else {
         setError(err.message);
+        }
       });
 
       // abort the fetch
