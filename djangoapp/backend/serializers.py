8294316778c60f53c968811d9c3964b4e3679829
Diogo Cruz <diogo.e.cruz@hotmail.com>
2022-05-28 14:07:54 +0100

    some changes

---
+++ djangoapp/backend/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+# from .models import Item
+
+# class ItemSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Item
+#         fields = ['id','title','description','price']
+#         # fields = '__all__'
+
+# class RequestFoodSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = RequestFood
+#         fields = ['title','price','locationtag','idimage']
---
+++ djangoapp/backend/urls.py
@@ -0,0 +1,13 @@
+import imp
+from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
+# from .views import ItemAPIView, RequestFoodAPIView
+
+urlpatterns = [
+    # path('items/', ItemAPIView.as_view()),
+    # path('requestfooditem/', RequestFoodAPIView.as_view()),
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    # path('red/<int:id>/<str:ola>', views.red, name='red'),
+    # path('sum/<int:sum1>/<int:sum2>', views.sum, name='sum'),
+]
