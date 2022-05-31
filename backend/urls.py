ec748fe85a192731e2db2148956bc63a20e8d8f1
Diogo Cruz <diogo.e.cruz@hotmail.com>
2022-05-31 22:51:34 +0100

    fix url

--- backend/urls.py
+++ backend/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('getitems/', ItemAPIView.as_view()),
     path('calcprice/', CalculatePriceAPIView.as_view()),
     path('requestfood/', RequestFoodAPIView.as_view()),
-    path('api/updateitems/',UpdateItemsFoodAPIView.as_view()),
+    path('updateitems/',UpdateItemsFoodAPIView.as_view()),
     path('getfood/', GetFoodAPIView.as_view()),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
